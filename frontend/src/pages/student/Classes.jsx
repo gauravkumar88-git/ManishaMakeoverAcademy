f466c59dcc72f44c0056dc083f16c439a776f30c
gauravkumar88-git <gauravkumarcse88@gmail.com>
2026-07-06 10:59:45 +0530

    Main Change in notes

--- frontend/src/pages/student/Classes.jsx
+++ frontend/src/pages/student/Classes.jsx
@@ -48,7 +48,7 @@ export default function StudentClasses() {
 
     return userPlan >= classPlan;
   };
-  console.log("USER DATA:", user);
+  // console.log("USER DATA:", user);
 
   useEffect(() => {
     const fetch = async () => {
@@ -298,37 +298,23 @@ export default function StudentClasses() {
                             View Plans
                           </Link>
                         )
-                     ) : cls.status === "completed" ? (
-  hasAccess ? (
-    <div className="flex gap-2">
-      {cls.recordingUrl && (
-        <a
-          href={cls.recordingUrl}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="flex-1 flex items-center justify-center gap-2 py-2.5 rounded-xl text-sm font-medium bg-purple-500/20 text-purple-400"
-        >
-          <FiVideo size={15} />
-          Recording
-        </a>
-      )}
-
-      <Link
-        to={`/student/notes/${cls._id}`}
-        className="flex-1 flex items-center justify-center gap-2 py-2.5 rounded-xl text-sm font-medium text-white"
-        style={{
-          background: "linear-gradient(135deg,#E91E8C,#9C27B0)",
-        }}
-      >
-        📄 View Notes
-      </Link>
-    </div>
+                     ) :  cls.status === "completed" ? (
+  cls.recordingUrl ? (
+    <a
+      href={cls.recordingUrl}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="flex-1 flex items-center justify-center gap-2 py-2.5 rounded-xl text-sm font-medium bg-purple-500/20 text-purple-400"
+    >
+      <FiVideo size={15} />
+      Recording
+    </a>
   ) : (
     <button
       disabled
       className="flex-1 py-2.5 rounded-xl text-sm font-medium bg-gray-500/10 text-gray-400"
     >
-      No Access
+      Completed
     </button>
   )
 ) : (
